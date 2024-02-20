bb0bb8d5480102e004081af2fc88d9fc6442befb
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-02-20 12:16:51 +0300

    Add project files.

---
+++ TodoApi/Controllers/HavaDurumuController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Dtos;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HavaDurumuController : ControllerBase
+    {
+        readonly Random rnd = new Random();
+        readonly string[] _durumlar = ["Açık", "Parçalı Bulutlu", "Çok Bulutlu", "Sisli", "Puslu", "Yağmurlu", "Sağanak Yağışlı", "Kar Yağışlı", "Karla Karışık Yağmur", "Dolu", "Sıcak", "Soğuk", "Rüzgarlı", "Fırtına", "Kasırga"];
+
+        [HttpGet]
+        public List<HavaDto> Get()
+        {
+            var tahminler = new List<HavaDto>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                tahminler.Add(new HavaDto()
+                {
+                    Tarih= DateOnly.FromDateTime(DateTime.Today.AddDays(i)),
+                    Sicaklik = rnd.Next(-5,31),
+                    Aciklama = _durumlar[rnd.Next(_durumlar.Length)]
+                });
+            }
+
+            return tahminler;
+        }
+    }
+}
